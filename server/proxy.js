5f3f870db1661560e6f4930bba40c93046e6a35e
Dondrey Taylor <dondrey.taylor@gmail.com>
2017-10-23 16:02:51 -0400

    Removed ssl and proxy from git ignore

---
+++ server/proxy.js
@@ -0,0 +1,21 @@
+var proxy = require('redbird')({
+    port: 80,
+    secure: true,
+    ssl: {
+        port: 443,
+        key: "./dnn_ssl/-.dnn.media_private_key.key",
+        cert: "./dnn_ssl/dnn.media_ssl_certificate.cer",
+    }
+});
+
+proxy.register("dnn.media", "http://localhost:8003", {ssl: true});
+proxy.register("slack.dnn.media", "http://localhost:8001", {ssl: true});
+
+proxy.register("dnnmedia.io", "http://localhost:8003", {ssl: false});
+proxy.register("slack.dnnmedia.io", "http://localhost:8001", {ssl: false});
+
+proxy.register("demo.dnn.media", "http://localhost:8003", {ssl: true});
+proxy.register("demo.dnnmedia.io", "http://localhost:8003", {ssl: false});
+
+proxy.register("crowdsale.dnn.media", "http://localhost:8004", {ssl: true});
+proxy.register("crowdsale.dnnmedia.io", "http://localhost:8004", {ssl: false});
